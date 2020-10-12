d31d6a61f94912b3016610410ad77efc7eb203dd
Seth Goodwin <skgoodwi@uncg.edu>
2020-10-12 19:15:17 -0400

    Upload UI

---
+++ Project Code/Swift/CovidLens/COVID Lens/Views/HomeView.swift
@@ -0,0 +1,58 @@
+//
+//  HomeView.swift
+//  COVID Lens
+//
+//  Created by Seth Goodwin on 10/8/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    // string array from database of of form: <affiliation> : <number of cases>
+    let data = ["Student", "13", "Faculty", "4", "Staff", "12", "Contractor", "1"]
+    
+    // two-column grid
+    let layout = [GridItem(.flexible()), GridItem(.flexible()) ]
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                //ScrollView {
+                Spacer()
+                    Text("Confirmed UNCG COVID-19 Cases")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 5)
+                    VStack {
+                        LazyVGrid(columns: layout, spacing: 30) {
+                            ForEach(data, id: \.self) { item in
+                                Text(item)
+                                    .font(.system(size: 18))
+                                    .foregroundColor(.black)
+                            }
+                            Text("Total")
+                                .font(.system(size: 18))
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                            Text("30")
+                                .font(.system(size: 18))
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                        }.padding(.vertical, 25)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 4)
+                        )
+                    }.padding(.horizontal, 20)
+                
+                .padding(.vertical, -8)
+                Spacer()
+                Divider()
+                //}
+            }.background(Color.white.ignoresSafeArea(.all, edges: .all))
+            .navigationBarTitle("Home", displayMode: .inline)
+        }
+    }
+}
