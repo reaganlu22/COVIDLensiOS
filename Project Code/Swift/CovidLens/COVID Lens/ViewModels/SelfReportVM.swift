a45a05ba7a0bbf4d321f377514c466aa8d243380
ReaganB <rtberhe@uncg.edu>
2020-10-28 18:21:13 -0400

    ..

--- Project Code/Swift/CovidLens/COVID Lens/ViewModels/SelfReportVM.swift
+++ Project Code/Swift/CovidLens/COVID Lens/ViewModels/SelfReportVM.swift
@@ -12,4 +12,5 @@ final class SelfReportVM: ObservableObject {
     let title = "Share A Positive Result"
     let info = "If you test positive for COVID-19, experience COVID-19 symptoms, or have been identified as a close contact of someone who has tested positive for COVID-19, please fill out and submit the following form."
     let icon = "checkmark.shield.fill"
+//    let URL_REPORT_CREATOR = "http://192.168.1.103/***/api/******.php"
 }
